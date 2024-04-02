4f2b26c5112275ad65948abbd459c427f5df04fa
Krik <kirkmears@gmail.com>
2024-04-01 19:12:29 -0500

    Update index.php
    added solar eclipse forecast link

--- index.php
+++ index.php
@@ -125,7 +125,7 @@
 <p style="text-align: center;"><a href="https://www.kirkmears.com/army" target="_blank">army</a></p>	    
 <p style="text-align: center;"><a href="http://192.168.0.239" target="_blank"><img src="icons/splash.jpg" alt="" width="80" height="80" align="middle"></a></p>
 <p style="text-align: center;"><img src="https://radar.weather.gov/ridge/standard/KFWS_loop.gif" alt="" width="60" height="55" align="middle">&nbsp;</p>
-
+<p style="text-align: center;"><a href="https://www.weather.gov/forecastpoints?lat=32.793333&lon=-96.766513&clat=32.793333&clon=-96.766513&zoom=16&bbox=[-19719439.353,1706090.691,-1372338.093,10673494.116]&layers=RangeRings,USStates,USCounties,ForecastPointPolygon,ForecastPoint,Domain,&order=12,5,6,0,1,4,7,10,11&obs=ttffffft&countyNames=t"  target="_blank">Eclipse Forecast</a></p>
 	<tr>
 
         </tr>
