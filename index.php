c1406e8625f59251fcc69025965c3cc067870d9d
Krik <kirkmears@gmail.com>
2026-08-12 16:53:46 -0500

    Update index.php
    correct aspect ratio of weather.html

--- index.php
+++ index.php
@@ -88,7 +88,7 @@
 
     <!-- Weather page: FIXED iframe (was unclosed + had no dimensions) -->
     <p style="text-align: center;">
-        <iframe src="https://kirkdotcom.azurewebsites.net/weather.html?embed=1" width="800" height="600" style="border: none;"></iframe>
+        <iframe src="https://kirkdotcom.azurewebsites.net/weather.html?embed=1" width="400" height="1200" style="border: none;"></iframe>
     </p>
     <p style="text-align: center;">
         <a href="https://kirkdotcom.azurewebsites.net/weather.html" target="_blank">Open weather page full-size</a>
