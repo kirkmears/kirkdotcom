fa677c7228cd8d28255cda32a99178181ad01ff0
Krik <kirkmears@gmail.com>
2026-08-12 18:12:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -88,7 +88,7 @@
 
     <!-- Weather page: FIXED iframe (was unclosed + had no dimensions) -->
     <p style="text-align: center;">
-        <iframe src="https://kirkdotcom.azurewebsites.net/weather.html?embed=1" width="1200" height="400" style="border: none;"></iframe>
+        <iframe src="https://kirkdotcom.azurewebsites.net/weather.html?embed=1" width="1000" height="450" style="border: none;"></iframe>
     </p>
     <p style="text-align: center;">
         <a href="https://kirkdotcom.azurewebsites.net/weather.html" target="_blank">Dallas Weather</a>
